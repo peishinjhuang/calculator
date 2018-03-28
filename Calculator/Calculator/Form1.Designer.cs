63dcdc1b5976a5524b3fe06b4c9a633f05e2b3e2
Pei Shin Jhuang <33123904+peishinjhuang@users.noreply.github.com>
2018-03-29 01:10:52 +0800

    Add files via upload

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.clearnBtn = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.equation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button9
+            // 
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // result
+            // 
+            resources.ApplyResources(this.result, "result");
+            this.result.Name = "result";
+            // 
+            // button10
+            // 
+            resources.ApplyResources(this.button10, "button10");
+            this.button10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button10.Name = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.operationBtn_click);
+            // 
+            // button11
+            // 
+            resources.ApplyResources(this.button11, "button11");
+            this.button11.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button11.Name = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.operationBtn_click);
+            // 
+            // button12
+            // 
+            resources.ApplyResources(this.button12, "button12");
+            this.button12.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button12.Name = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.operationBtn_click);
+            // 
+            // button13
+            // 
+            resources.ApplyResources(this.button13, "button13");
+            this.button13.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button13.Name = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.operationBtn_click);
+            // 
+            // button14
+            // 
+            resources.ApplyResources(this.button14, "button14");
+            this.button14.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button14.Name = "button14";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.getResult);
+            // 
+            // button16
+            // 
+            resources.ApplyResources(this.button16, "button16");
+            this.button16.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button16.Name = "button16";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button15
+            // 
+            resources.ApplyResources(this.button15, "button15");
+            this.button15.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button15.Name = "button15";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.floatBtn_Click);
+            // 
+            // clearnBtn
+            // 
+            resources.ApplyResources(this.clearnBtn, "clearnBtn");
+            this.clearnBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.clearnBtn.Name = "clearnBtn";
+            this.clearnBtn.UseVisualStyleBackColor = true;
+            this.clearnBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // button17
+            // 
+            resources.ApplyResources(this.button17, "button17");
+            this.button17.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button17.Name = "button17";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.removeNum);
+            // 
+            // equation
+            // 
+            resources.ApplyResources(this.equation, "equation");
+            this.equation.Name = "equation";
+            // 
+            // Calculator
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Controls.Add(this.equation);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.clearnBtn);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button clearnBtn;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Label equation;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        double total = 0; // 總值
+        string operation = ""; // 運算符號
+        bool operationBtn_pressed = false; 
+        bool multiOperation = false; //按運算時，算出前面算式的值
+        bool enterBtn_pressed = false;
+        bool removeBtn_pressed = false;
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)　// 數值鈕方法
+        {
+            if (result.Text == "0" || operationBtn_pressed == true) //去除一開始的0或刪除前次數值
+            {
+                result.Clear();
+            }
+            Button btn = (Button)sender;  // 轉型並讀取參數
+            result.Text +=  btn.Text; // 改變提示框的字
+            operationBtn_pressed = false;
+        }
+
+        private void floatBtn_Click(object sender, EventArgs e)　// 小數點鈕方法
+        {
+            Button btn = (Button)sender;  // 轉型並讀取參數
+            if (operationBtn_pressed == false) // 前次輸入為數值鈕
+            {
+                result.Text += btn.Text; 
+            }
+            if (enterBtn_pressed == true ) // 前次輸入為enter，總值為字框裡的值
+            {
+                total = Double.Parse(result.Text);
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            total = 0;
+            result.Text = "0";
+            equation.Text = "";
+        }
+
+        private void operationBtn_click(object sender, EventArgs e) //按運算鈕時，計算前次的值
+        {
+           
+            if (multiOperation == false || enterBtn_pressed == true) // 未按過運算鈕或已按enter，總值為字框裡的值
+            {
+                total = Double.Parse(result.Text);
+            }
+            else if (multiOperation == true) // 先前已按過運算鈕，計算前次的值
+            {
+                switch (operation)
+                {
+                    case "+":
+                        total +=  Double.Parse(result.Text);
+                        break;
+                    case "-":
+                        total -=  Double.Parse(result.Text);
+                        break;
+                    case "*":
+                        total *=  Double.Parse(result.Text);
+                        break;
+                    case "/":
+                        total /=  Double.Parse(result.Text);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            operationBtn_pressed = true;
+            enterBtn_pressed = false; 
+            multiOperation = true; // 已按過運算鈕
+            Button btn = (Button)sender;
+            operation = btn.Text; // 儲存當前的運算鈕供下次使用
+            equation.Text += result.Text + btn.Text;
+        }
+
+        private void getResult(object sender, EventArgs e)
+        {
+            if (removeBtn_pressed == true && result.Text.Length == 0) // remove當前全部的值
+            {
+                result.Text = total.ToString();
+            }
+            else if (enterBtn_pressed == false) // 重複按enter，不會改變值
+            {
+                enterBtn_pressed = true;
+                switch (operation)
+                {
+                    case "+":
+                        total += Double.Parse(result.Text);
+                        break;
+                    case "-":
+                        total -= Double.Parse(result.Text);
+                        break;
+                    case "*":
+                        total *= Double.Parse(result.Text);
+                        break;
+                    case "/":
+                        total /= Double.Parse(result.Text);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            result.Text = total.ToString();
+            equation.Text = "";
+            operation = "";
+        }
+
+        private void removeNum(object sender, EventArgs e)
+        {
+            if (result.Text.Length != 0)　// 從字尾刪除數字
+            {
+                result.Text = result.Text.Remove(result.Text.Length - 1);
+            }
+            if (result.Text.Length == 0)
+            {
+                result.Text = "0"; // 全部數字刪除後，將值設為０
+            }
+            removeBtn_pressed = true;
+        }
+    }
+}
